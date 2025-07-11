421d662ae965e313bab8211bbb809eaaf4c66672
birdyxvibe <birdthebirb2@gmail.com>
2025-07-10 22:00:19 -0500

    feat(chat): updated color hexes & names

--- src/main/java/com/birdy/fragileLife/chat/ChatColors.java
+++ src/main/java/com/birdy/fragileLife/chat/ChatColors.java
@@ -24,16 +24,16 @@ public class ChatColors {
         COLORS.put("White", new ColorOption(TextColor.fromHexString("#FFFFFF"), Material.WHITE_CONCRETE));
         COLORS.put("Charcoal", new ColorOption(TextColor.fromHexString("#575757"), Material.DEEPSLATE_TILES));
         COLORS.put("Taupe", new ColorOption(TextColor.fromHexString("#CCAC93"), Material.PACKED_MUD));
-        COLORS.put("Tomato", new ColorOption(TextColor.fromHexString("#d95d4e"), Material.NETHER_WART_BLOCK));
-        COLORS.put("Citrus", new ColorOption(TextColor.fromHexString("#E86C28"), Material.ORANGE_TERRACOTTA));
-        COLORS.put("Gold", new ColorOption(TextColor.fromHexString("#CFAB1F"), Material.GOLD_BLOCK));
+        COLORS.put("Rose", new ColorOption(TextColor.fromHexString("#E3242B"), Material.NETHER_WART_BLOCK));
+        COLORS.put("Tangerine", new ColorOption(TextColor.fromHexString("#FA8128"), Material.ORANGE_TERRACOTTA));
+        COLORS.put("Honey", new ColorOption(TextColor.fromHexString("#ecc734"), Material.HONEY_BLOCK));
         COLORS.put("Olive", new ColorOption(TextColor.fromHexString("#AFB83B"), Material.MOSS_BLOCK));
-        COLORS.put("Pine", new ColorOption(TextColor.fromHexString("#279228"), Material.DARK_OAK_LEAVES));
+        COLORS.put("Shamrock", new ColorOption(TextColor.fromHexString("#03AC13"), Material.JUNGLE_LEAVES));
         COLORS.put("Mint", new ColorOption(TextColor.fromHexString("#98FF98"), Material.SLIME_BLOCK));
-        COLORS.put("Teal", new ColorOption(TextColor.fromHexString("#17c7ea"), Material.LIGHT_BLUE_CONCRETE_POWDER));
-        COLORS.put("Ocean", new ColorOption(TextColor.fromHexString("#89acff"), Material.STRIPPED_WARPED_HYPHAE));
-        COLORS.put("Lilac", new ColorOption(TextColor.fromHexString("#C8A2C8"), Material.PINK_CONCRETE_POWDER));
-        COLORS.put("Berry", new ColorOption(TextColor.fromHexString("#B8255F"), Material.MAGENTA_CONCRETE));
+        COLORS.put("Sapphire", new ColorOption(TextColor.fromHexString("#0492C2"), Material.CYAN_GLAZED_TERRACOTTA));
+        COLORS.put("Teal", new ColorOption(TextColor.fromHexString("#48AAAD"), Material.LIGHT_BLUE_CONCRETE_POWDER));
+        COLORS.put("Flamingo", new ColorOption(TextColor.fromHexString("#FDA4BA"), Material.PINK_CONCRETE_POWDER));
+        COLORS.put("Taffy", new ColorOption(TextColor.fromHexString("#FC46AA"), Material.MAGENTA_CONCRETE));
         COLORS.put("Grape", new ColorOption(TextColor.fromHexString("#884DFF"), Material.AMETHYST_BLOCK));
     }
 }
